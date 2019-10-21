09e4b40aa331cc3719d93cab2ac737ad92b0598d
Conrad Hoffmann <ch@bitfehler.net>
2019-10-21 14:50:45 +0200

    Run growpart via cloud-init instead of service
    The new cloud.cfg is generated for Arch Linux from the current
    cloud-init master branch, which gained better support for Arch Linux. In
    general, enabling more modules should make the cloud.cfg work better for
    most folks.

--- provision.sh
+++ provision.sh
@@ -55,13 +55,8 @@ echo "archec2" | sudo tee -a arch/mnt/etc/hostname
 sudo cp arch/*.pkg.tar.xz arch/mnt/
 sudo chroot arch arch-chroot /mnt pacman -U --noconfirm /growpart.pkg.tar.xz
 sudo chroot arch arch-chroot /mnt pacman -U --noconfirm /netplan.pkg.tar.xz
+sudo mv arch/archlinux.cloud.cfg arch/mnt/etc/cloud/cloud.cfg
 sudo rm arch/mnt/*.pkg.tar.xz
-
-# Enable the growpart service. This will resize both the main partition as well
-# as the file system it contains to use all available space. Without this, the
-# main file system would always only be ~4GB, even if you start an instance
-# with larger EBS volumes attached.
-sudo chroot arch arch-chroot /mnt systemctl enable growpartfs@-
 
 # Enable cloud-init services. This will set the hostname, but also do more
 # advanced things like setting up the SSH key that you select for your
